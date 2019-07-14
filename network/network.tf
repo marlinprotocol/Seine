371c18dd7e123bd3ee1dfb6a7c1a6fbb56abe8da
Roshan Raghupathy <thisisroshansmail@gmail.com>
2019-07-14 10:16:01 +0530

    Add subnet inside VPC

--- network/network.tf
+++ network/network.tf
@@ -3,3 +3,10 @@ resource "aws_vpc" "default" {
   cidr_block           = "10.0.0.0/16"
   enable_dns_hostnames = true
 }
+
+# Subnet
+resource "aws_subnet" "default" {
+  vpc_id                  = "${aws_vpc.default.id}"
+  cidr_block              = "${aws_vpc.default.cidr_block}"
+  map_public_ip_on_launch = true
+}
