182a11af140ed91160a3608da6b9561241df53fb
Roshan Raghupathy <thisisroshansmail@gmail.com>
2019-07-14 10:44:15 +0530

    Hotfix: Route tables need to be associated with a subnet to work

--- network/network.tf
+++ network/network.tf
@@ -16,6 +16,12 @@ resource "aws_route_table" "default" {
   vpc_id = "${aws_vpc.default.id}"
 }
 
+# Route table association
+resource "aws_route_table_association" "default" {
+  subnet_id      = "${aws_subnet.default.id}"
+  route_table_id = "${aws_route_table.default.id}"
+}
+
 # Internet gateway
 resource "aws_internet_gateway" "default" {
   vpc_id = "${aws_vpc.default.id}"
