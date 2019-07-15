d78b7a32334b90f6305f606e3c062126842d5b73
Roshan Raghupathy <thisisroshansmail@gmail.com>
2019-07-15 15:53:36 +0530

    Try deploying VPCs in different regions

--- network/network.tf
+++ network/network.tf
@@ -1,7 +1,14 @@
+# Provider
+provider "aws" {
+  alias  = "ap-south-1"
+  region = "ap-south-1"
+}
+
 # VPC
 resource "aws_vpc" "default" {
   cidr_block           = "10.0.0.0/16"
   enable_dns_hostnames = true
+  provider             = "aws.ap-south-1"
 }
 
 # Subnet
@@ -9,22 +16,26 @@ resource "aws_subnet" "default" {
   vpc_id                  = "${aws_vpc.default.id}"
   cidr_block              = "${aws_vpc.default.cidr_block}"
   map_public_ip_on_launch = true
+  provider                = "aws.ap-south-1"
 }
 
 # Route table
 resource "aws_route_table" "default" {
-  vpc_id = "${aws_vpc.default.id}"
+  vpc_id   = "${aws_vpc.default.id}"
+  provider = "aws.ap-south-1"
 }
 
 # Route table association
 resource "aws_route_table_association" "default" {
   subnet_id      = "${aws_subnet.default.id}"
   route_table_id = "${aws_route_table.default.id}"
+  provider       = "aws.ap-south-1"
 }
 
 # Internet gateway
 resource "aws_internet_gateway" "default" {
-  vpc_id = "${aws_vpc.default.id}"
+  vpc_id   = "${aws_vpc.default.id}"
+  provider = "aws.ap-south-1"
 }
 
 # Internet route
@@ -32,13 +43,21 @@ resource "aws_route" "internet" {
   route_table_id         = "${aws_route_table.default.id}"
   destination_cidr_block = "0.0.0.0/0"
   gateway_id             = "${aws_internet_gateway.default.id}"
+  provider               = "aws.ap-south-1"
 }
 
+
+# Provider
+provider "aws" {
+  alias  = "ap-southeast-1"
+  region = "ap-southeast-1"
+}
 
 # VPC
 resource "aws_vpc" "default2" {
   cidr_block           = "10.1.0.0/16"
   enable_dns_hostnames = true
+  provider             = "aws.ap-southeast-1"
 }
 
 # Subnet
@@ -46,22 +65,26 @@ resource "aws_subnet" "default2" {
   vpc_id                  = "${aws_vpc.default2.id}"
   cidr_block              = "${aws_vpc.default2.cidr_block}"
   map_public_ip_on_launch = true
+  provider                = "aws.ap-southeast-1"
 }
 
 # Route table
 resource "aws_route_table" "default2" {
-  vpc_id = "${aws_vpc.default2.id}"
+  vpc_id   = "${aws_vpc.default2.id}"
+  provider = "aws.ap-southeast-1"
 }
 
 # Route table association
 resource "aws_route_table_association" "default2" {
   subnet_id      = "${aws_subnet.default2.id}"
   route_table_id = "${aws_route_table.default2.id}"
+  provider       = "aws.ap-southeast-1"
 }
 
 # Internet gateway
 resource "aws_internet_gateway" "default2" {
-  vpc_id = "${aws_vpc.default2.id}"
+  vpc_id   = "${aws_vpc.default2.id}"
+  provider = "aws.ap-southeast-1"
 }
 
 # Internet route
@@ -69,4 +92,5 @@ resource "aws_route" "internet" {
   route_table_id         = "${aws_route_table.default2.id}"
   destination_cidr_block = "0.0.0.0/0"
   gateway_id             = "${aws_internet_gateway.default2.id}"
+  provider               = "aws.ap-southeast-1"
 }
