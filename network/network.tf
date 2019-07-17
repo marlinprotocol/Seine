34c3ccfa0d6d1d8710d13e12cfc5124df0420d11
Roshan Raghupathy <thisisroshansmail@gmail.com>
2019-07-17 12:05:20 +0530

    Replace the Mumbai VPC with the module

--- network/network.tf
+++ network/network.tf
@@ -1,49 +1,6 @@
-# Provider
-provider "aws" {
-  alias  = "ap-south-1"
-  region = "ap-south-1"
-}
-
-# VPC
-resource "aws_vpc" "default" {
-  cidr_block           = "10.0.0.0/16"
-  enable_dns_hostnames = true
-  provider             = "aws.ap-south-1"
-}
-
-# Subnet
-resource "aws_subnet" "default" {
-  vpc_id                  = "${aws_vpc.default.id}"
-  cidr_block              = "${aws_vpc.default.cidr_block}"
-  map_public_ip_on_launch = true
-  provider                = "aws.ap-south-1"
-}
-
-# Route table
-resource "aws_route_table" "default" {
-  vpc_id   = "${aws_vpc.default.id}"
-  provider = "aws.ap-south-1"
-}
-
-# Route table association
-resource "aws_route_table_association" "default" {
-  subnet_id      = "${aws_subnet.default.id}"
-  route_table_id = "${aws_route_table.default.id}"
-  provider       = "aws.ap-south-1"
-}
-
-# Internet gateway
-resource "aws_internet_gateway" "default" {
-  vpc_id   = "${aws_vpc.default.id}"
-  provider = "aws.ap-south-1"
-}
-
-# Internet route
-resource "aws_route" "internet" {
-  route_table_id         = "${aws_route_table.default.id}"
-  destination_cidr_block = "0.0.0.0/0"
-  gateway_id             = "${aws_internet_gateway.default.id}"
-  provider               = "aws.ap-south-1"
+# Network
+module "mumbai_network" {
+  source = "./modules/mumbai_network"
 }
 
 
