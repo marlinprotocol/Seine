bcd8eec0d61413a9f6e550cb8a5e6d28f7d67ef9
Roshan Raghupathy <thisisroshansmail@gmail.com>
2019-07-18 16:27:24 +0530

    Replace Singapore VPC in main file with module

--- network/network.tf
+++ network/network.tf
@@ -1,57 +1,12 @@
-# Network
+# Mumbai network
 module "mumbai_network" {
   source = "./modules/mumbai_network"
 }
 
-
-# Provider
-provider "aws" {
-  alias  = "ap-southeast-1"
-  region = "ap-southeast-1"
+# Singapore network
+module "singapore_network" {
+  source = "./modules/singapore_network"
 }
-
-# VPC
-resource "aws_vpc" "default2" {
-  cidr_block           = "10.1.0.0/16"
-  enable_dns_hostnames = true
-  provider             = "aws.ap-southeast-1"
-}
-
-# Subnet
-resource "aws_subnet" "default2" {
-  vpc_id                  = "${aws_vpc.default2.id}"
-  cidr_block              = "${aws_vpc.default2.cidr_block}"
-  map_public_ip_on_launch = true
-  provider                = "aws.ap-southeast-1"
-}
-
-# Route table
-resource "aws_route_table" "default2" {
-  vpc_id   = "${aws_vpc.default2.id}"
-  provider = "aws.ap-southeast-1"
-}
-
-# Route table association
-resource "aws_route_table_association" "default2" {
-  subnet_id      = "${aws_subnet.default2.id}"
-  route_table_id = "${aws_route_table.default2.id}"
-  provider       = "aws.ap-southeast-1"
-}
-
-# Internet gateway
-resource "aws_internet_gateway" "default2" {
-  vpc_id   = "${aws_vpc.default2.id}"
-  provider = "aws.ap-southeast-1"
-}
-
-# Internet route
-resource "aws_route" "internet" {
-  route_table_id         = "${aws_route_table.default2.id}"
-  destination_cidr_block = "0.0.0.0/0"
-  gateway_id             = "${aws_internet_gateway.default2.id}"
-  provider               = "aws.ap-southeast-1"
-}
-
 
 # Peering
 resource "aws_vpc_peering_connection" "default" {
