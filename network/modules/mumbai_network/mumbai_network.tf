047c46d833237315cfee2b68439233582f3eb07f
Roshan Raghupathy <thisisroshansmail@gmail.com>
2019-07-21 16:26:45 +0530

    Add tags to resources

--- network/modules/mumbai_network/mumbai_network.tf
+++ network/modules/mumbai_network/mumbai_network.tf
@@ -9,6 +9,10 @@ resource "aws_vpc" "default" {
   cidr_block           = "10.0.0.0/16"
   enable_dns_hostnames = true
   provider             = "aws.ap-south-1"
+
+  tags = {
+    project = "marlin-terraform"
+  }
 }
 
 # Subnet
@@ -17,12 +21,20 @@ resource "aws_subnet" "default" {
   cidr_block              = "${aws_vpc.default.cidr_block}"
   map_public_ip_on_launch = true
   provider                = "aws.ap-south-1"
+
+  tags = {
+    project = "marlin-terraform"
+  }
 }
 
 # Route table
 resource "aws_route_table" "default" {
   vpc_id   = "${aws_vpc.default.id}"
   provider = "aws.ap-south-1"
+
+  tags = {
+    project = "marlin-terraform"
+  }
 }
 
 # Route table association
@@ -36,6 +48,10 @@ resource "aws_route_table_association" "default" {
 resource "aws_internet_gateway" "default" {
   vpc_id   = "${aws_vpc.default.id}"
   provider = "aws.ap-south-1"
+
+  tags = {
+    project = "marlin-terraform"
+  }
 }
 
 # Internet route
