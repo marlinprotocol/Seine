471fa03b9dcbe76d8e55c5fd03d942139325dcb8
Roshan Raghupathy <thisisroshansmail@gmail.com>
2019-07-23 19:09:16 +0530

    Use the new common network module

--- network/network.tf
+++ network/network.tf
@@ -12,19 +12,31 @@ provider "aws" {
 
 # Mumbai network
 module "mumbai_network" {
-  source = "./modules/mumbai_network"
+  source    = "./modules/regional_network"
+  project   = "marlin-terraform"
+  vpc_block = "10.0.0.0/16"
+
+  providers = {
+    aws = "aws.ap-south-1"
+  }
 }
 
 # Singapore network
 module "singapore_network" {
-  source = "./modules/singapore_network"
+  source    = "./modules/regional_network"
+  project   = "marlin-terraform"
+  vpc_block = "10.1.0.0/16"
+
+  providers = {
+    aws = "aws.ap-southeast-1"
+  }
 }
 
 # Peering
 resource "aws_vpc_peering_connection" "default" {
   provider    = "aws.ap-south-1"
-  vpc_id      = "${module.mumbai_network.vpc_id}"
-  peer_vpc_id = "${module.singapore_network.vpc_id}"
+  vpc_id      = "${module.mumbai_network.vpc.id}"
+  peer_vpc_id = "${module.singapore_network.vpc.id}"
   peer_region = "ap-southeast-1"
 }
 
@@ -37,14 +49,14 @@ resource "aws_vpc_peering_connection_accepter" "default" {
 # Peering routes
 resource "aws_route" "mumbai_to_singapore" {
   provider                  = "aws.ap-south-1"
-  route_table_id            = "${module.mumbai_network.route_table_id}"
+  route_table_id            = "${module.mumbai_network.route_table.id}"
   destination_cidr_block    = "10.1.0.0/16"
   vpc_peering_connection_id = "${aws_vpc_peering_connection.default.id}"
 }
 
 resource "aws_route" "singapore_to_mumbai" {
   provider                  = "aws.ap-southeast-1"
-  route_table_id            = "${module.singapore_network.route_table_id}"
+  route_table_id            = "${module.singapore_network.route_table.id}"
   destination_cidr_block    = "10.0.0.0/16"
-  vpc_peering_connection_id = "${aws_vpc_peering_connection.default2.id}"
+  vpc_peering_connection_id = "${aws_vpc_peering_connection.default.id}"
 }
