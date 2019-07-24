ae180b5d02e03ce2f218cb662ada482efb7af801
Roshan Raghupathy <thisisroshansmail@gmail.com>
2019-07-24 13:27:29 +0530

    Start putting peering connection into a module

---
+++ network/modules/peering_connection/peering_connection.tf
@@ -0,0 +1,28 @@
+# Peering
+resource "aws_vpc_peering_connection" "default" {
+  provider    = "aws.ap-south-1"
+  vpc_id      = "${module.mumbai_network.vpc.id}"
+  peer_vpc_id = "${module.singapore_network.vpc.id}"
+  peer_region = "ap-southeast-1"
+}
+
+resource "aws_vpc_peering_connection_accepter" "default" {
+  provider                  = "aws.ap-southeast-1"
+  vpc_peering_connection_id = "${aws_vpc_peering_connection.default.id}"
+  auto_accept               = true
+}
+
+# Peering routes
+resource "aws_route" "mumbai_to_singapore" {
+  provider                  = "aws.ap-south-1"
+  route_table_id            = "${module.mumbai_network.route_table.id}"
+  destination_cidr_block    = "10.1.0.0/16"
+  vpc_peering_connection_id = "${aws_vpc_peering_connection.default.id}"
+}
+
+resource "aws_route" "singapore_to_mumbai" {
+  provider                  = "aws.ap-southeast-1"
+  route_table_id            = "${module.singapore_network.route_table.id}"
+  destination_cidr_block    = "10.0.0.0/16"
+  vpc_peering_connection_id = "${aws_vpc_peering_connection.default.id}"
+}
