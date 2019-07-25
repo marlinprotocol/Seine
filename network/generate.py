4f0ae04905612bfbd07e758e30833bc1c7a7828d
Roshan Raghupathy <thisisroshansmail@gmail.com>
2019-07-25 17:52:56 +0530

    Add generator script to generate tf file from j2 template

---
+++ network/generate.py
@@ -0,0 +1,12 @@
+#!/usr/bin/env python
+from jinja2 import Environment, FileSystemLoader
+import os
+env = Environment(loader=FileSystemLoader(os.path.join(os.path.dirname(__file__))))
+
+template = env.get_template('network.j2')
+output_from_parsed_template = template.render()
+# print(output_from_parsed_template)
+
+# to save the results
+with open("network.tf", "w") as fh:
+    fh.write(output_from_parsed_template)
