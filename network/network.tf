125822dcdda2bed3a6c181a6bf12de9fd48078fc
Roshan Raghupathy <thisisroshansmail@gmail.com>
2019-07-27 13:23:46 +0530

    Commit generated tf file

--- network/network.tf
+++ network/network.tf
@@ -4,6 +4,8 @@ variable "project" {
   description = "Project name"
 }
 
+################ ap-south-1 begin ################
+
 # Provider
 provider "aws" {
   alias  = "ap-south-1"
@@ -11,7 +13,7 @@ provider "aws" {
 }
 
 # ap-south-1 network
-module "ap-south-1-network" {
+module "ap-south-1" {
   source    = "./modules/regional_network"
   project   = "${var.project}"
   vpc_block = "10.0.0.0/16"
@@ -21,6 +23,28 @@ module "ap-south-1-network" {
   }
 }
 
+# Peering with ap-southeast-1
+module "ap-south-1-ap-southeast-1" {
+  source = "./modules/peering_connection"
+  project = "${var.project}"
+  src_vpc_id = module.ap-south-1.vpc.id
+  dst_vpc_id = module.ap-southeast-1.vpc.id
+  src_route_table_id = module.ap-south-1.route_table.id
+  dst_route_table_id = module.ap-southeast-1.route_table.id
+  src_cidr = "10.0.0.0/16"
+  dst_cidr = "10.1.0.0/16"
+
+  providers = {
+    aws.src = "aws.ap-south-1"
+    aws.dst = "aws.ap-southeast-1"
+  }
+}
+
+################ ap-south-1 end ################
+
+
+################ ap-southeast-1 begin ################
+
 # Provider
 provider "aws" {
   alias  = "ap-southeast-1"
@@ -28,7 +52,7 @@ provider "aws" {
 }
 
 # ap-southeast-1 network
-module "ap-southeast-1-network" {
+module "ap-southeast-1" {
   source    = "./modules/regional_network"
   project   = "${var.project}"
   vpc_block = "10.1.0.0/16"
@@ -38,19 +62,6 @@ module "ap-southeast-1-network" {
   }
 }
 
-# Peering
-module "mumbai-singapore" {
-  source             = "./modules/peering_connection"
-  project            = "${var.project}"
-  src_vpc_id         = module.ap-south-1-network.vpc.id
-  dst_vpc_id         = module.ap-southeast-1-network.vpc.id
-  src_route_table_id = module.ap-south-1-network.route_table.id
-  dst_route_table_id = module.ap-southeast-1-network.route_table.id
-  src_cidr           = "10.0.0.0/16"
-  dst_cidr           = "10.1.0.0/16"
+################ ap-southeast-1 end ################
 
-  providers = {
-    aws.src = "aws.ap-south-1"
-    aws.dst = "aws.ap-southeast-1"
-  }
-}
+
