30ba742308b6e9a3c6e0957c2d58b36610578267
Roshan Raghupathy <thisisroshansmail@gmail.com>
2019-07-27 09:08:06 +0530

    Keep newline at end of file after generation

--- network/generate.py
+++ network/generate.py
@@ -1,7 +1,10 @@
 #!/usr/bin/env python
 from jinja2 import Environment, FileSystemLoader
 import os
-env = Environment(loader=FileSystemLoader(os.path.join(os.path.dirname(__file__))))
+env = Environment(
+	loader=FileSystemLoader(os.path.join(os.path.dirname(__file__))),
+	keep_trailing_newline=True,
+)
 
 template = env.get_template('network.j2')
 output_from_parsed_template = template.render(
