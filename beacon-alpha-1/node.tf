f44f01933e49ddc8b2267b4d3cd046ed009e62e5
Roshan Raghupathy <thisisroshansmail@gmail.com>
2019-08-02 18:53:03 +0530

    Get key as input parameter

--- beacon-alpha-1/node.tf
+++ beacon-alpha-1/node.tf
@@ -4,6 +4,11 @@ variable "project" {
   description = "Project name"
 }
 
+variable "key_name" {
+  type        = string
+  description = "SSH key name"
+}
+
 # AMI
 data "aws_ami" "ubuntu" {
   most_recent = true
@@ -30,7 +35,7 @@ data "aws_ami" "ubuntu" {
 resource "aws_instance" "beacon" {
   ami                    = "${data.aws_ami.ubuntu.id}"
   instance_type          = "t3.micro"
-  key_name               = ""
+  key_name               = "${var.key_name}"
   vpc_security_group_ids = ""
   subnet_id              = ""
   iam_instance_profile   = ""
