3ff901d3ba085ffab47f9ceed12a5c6b10871d67
Roshan Raghupathy <thisisroshansmail@gmail.com>
2019-08-02 21:48:26 +0530

    Get security groups as input

--- beacon-alpha-1/node.tf
+++ beacon-alpha-1/node.tf
@@ -9,6 +9,12 @@ variable "key_name" {
   description = "SSH key name"
 }
 
+variable "security_group_ids" {
+  type        = list(string)
+  description = "List of security group ids to add"
+  default     = []
+}
+
 # AMI
 data "aws_ami" "ubuntu" {
   most_recent = true
@@ -36,7 +42,7 @@ resource "aws_instance" "beacon" {
   ami                    = "${data.aws_ami.ubuntu.id}"
   instance_type          = "t3.micro"
   key_name               = "${var.key_name}"
-  vpc_security_group_ids = ""
+  vpc_security_group_ids = var.security_group_ids
   subnet_id              = ""
   iam_instance_profile   = ""
 
