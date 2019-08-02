240127a008b7d66a2fa8a30cacc354b4577d115e
Roshan Raghupathy <thisisroshansmail@gmail.com>
2019-08-02 18:14:33 +0530

    Add basic EC2 instance, most fields missing

--- beacon-alpha-1/node.tf
+++ beacon-alpha-1/node.tf
@@ -3,3 +3,23 @@ variable "project" {
   type        = string
   description = "Project name"
 }
+
+# Instance
+resource "aws_instance" "beacon" {
+  ami                    = ""
+  instance_type          = "t3.micro"
+  key_name               = ""
+  vpc_security_group_ids = ""
+  subnet_id              = ""
+  iam_instance_profile   = ""
+
+  tags = {
+    project = "${var.project}"
+    role    = "beacon"
+  }
+
+  volume_tags = {
+    project = "${var.project}"
+    role    = "beacon"
+  }
+}
