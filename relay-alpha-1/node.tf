c4bad2094ca21dfe9eae2be5ec084636311f2454
Roshan Raghupathy <thisisroshansmail@gmail.com>
2019-08-08 13:08:41 +0530

    Get instance profile from outside so multiple instances can share it

--- relay-alpha-1/node.tf
+++ relay-alpha-1/node.tf
@@ -20,6 +20,11 @@ variable "subnet_id" {
   description = "Subnet id where instance should be provisioned"
 }
 
+variable "iam_instance_profile" {
+  type        = string
+  description = "IAM instance profile"
+}
+
 # Outputs
 output "instance" {
   value = aws_instance.relay
@@ -47,48 +52,22 @@ data "aws_ami" "ubuntu" {
   owners = ["099720109477"] # Canonical
 }
 
-# IAM role
-resource "aws_iam_role" "relay" {
-  name = "${var.project}_relay"
-
-  assume_role_policy = <<EOF
-{
-  "Version": "2012-10-17",
-  "Statement": [
-    {
-      "Effect": "Allow",
-      "Principal": {
-        "Service": "ec2.amazonaws.com"
-      },
-      "Action": "sts:AssumeRole"
-    }
-  ]
-}
-EOF
-}
-
-# IAM profile
-resource "aws_iam_instance_profile" "relay" {
-  name = "${var.project}_relay"
-  role = "${aws_iam_role.relay.name}"
-}
-
 # Instance
 resource "aws_instance" "relay" {
-  ami = "${data.aws_ami.ubuntu.id}"
-  instance_type = "c5.large"
-  key_name = "${var.key_name}"
+  ami                    = "${data.aws_ami.ubuntu.id}"
+  instance_type          = "c5.large"
+  key_name               = "${var.key_name}"
   vpc_security_group_ids = var.security_group_ids
-  subnet_id = "${var.subnet_id}"
-  iam_instance_profile = "${aws_iam_instance_profile.relay.name}"
+  subnet_id              = "${var.subnet_id}"
+  iam_instance_profile   = var.iam_instance_profile
 
   tags = {
     project = "${var.project}"
-    role = "relay"
+    role    = "relay"
   }
 
   volume_tags = {
     project = "${var.project}"
-    role = "relay"
+    role    = "relay"
   }
 }
