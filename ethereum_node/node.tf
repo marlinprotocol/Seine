c09805e81c57856f02fe04f5ca1e4fae50a27467
Roshan Raghupathy <thisisroshansmail@gmail.com>
2019-08-12 09:42:17 +0530

    Start a new ethereum node module

---
+++ ethereum_node/node.tf
@@ -0,0 +1,94 @@
+# Inputs
+variable "project" {
+  type        = string
+  description = "Project name"
+}
+
+variable "key_name" {
+  type        = string
+  description = "SSH key name"
+}
+
+variable "security_group_ids" {
+  type        = list(string)
+  description = "List of security group ids to add"
+  default     = []
+}
+
+variable "subnet_id" {
+  type        = string
+  description = "Subnet id where instance should be provisioned"
+}
+
+# Outputs
+output "instance" {
+  value = aws_instance.beacon
+}
+
+# AMI
+data "aws_ami" "ubuntu" {
+  most_recent = true
+
+  filter {
+    name   = "name"
+    values = ["ubuntu/images/hvm-ssd/ubuntu-bionic-18.04-amd64-server-*"]
+  }
+
+  filter {
+    name   = "root-device-type"
+    values = ["ebs"]
+  }
+
+  filter {
+    name   = "virtualization-type"
+    values = ["hvm"]
+  }
+
+  owners = ["099720109477"] # Canonical
+}
+
+# IAM role
+resource "aws_iam_role" "beacon" {
+  name = "${var.project}_beacon"
+
+  assume_role_policy = <<EOF
+{
+  "Version": "2012-10-17",
+  "Statement": [
+    {
+      "Effect": "Allow",
+      "Principal": {
+        "Service": "ec2.amazonaws.com"
+      },
+      "Action": "sts:AssumeRole"
+    }
+  ]
+}
+EOF
+}
+
+# IAM profile
+resource "aws_iam_instance_profile" "beacon" {
+  name = "${var.project}_beacon"
+  role = "${aws_iam_role.beacon.name}"
+}
+
+# Instance
+resource "aws_instance" "beacon" {
+  ami = "${data.aws_ami.ubuntu.id}"
+  instance_type = "t3.micro"
+  key_name = "${var.key_name}"
+  vpc_security_group_ids = var.security_group_ids
+  subnet_id = "${var.subnet_id}"
+  iam_instance_profile = "${aws_iam_instance_profile.beacon.name}"
+
+  tags = {
+    project = "${var.project}"
+    role = "beacon"
+  }
+
+  volume_tags = {
+    project = "${var.project}"
+    role = "beacon"
+  }
+}
