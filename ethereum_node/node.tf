b87c0815cba8e432dc9ccfc6ab583882306c4610
Roshan Raghupathy <thisisroshansmail@gmail.com>
2019-08-12 10:23:24 +0530

    Copy from relay and rename relay -> eth

--- ethereum_node/node.tf
+++ ethereum_node/node.tf
@@ -20,9 +20,14 @@ variable "subnet_id" {
   description = "Subnet id where instance should be provisioned"
 }
 
+variable "iam_instance_profile" {
+  type        = string
+  description = "IAM instance profile"
+}
+
 # Outputs
 output "instance" {
-  value = aws_instance.beacon
+  value = aws_instance.eth
 }
 
 # AMI
@@ -47,48 +52,22 @@ data "aws_ami" "ubuntu" {
   owners = ["099720109477"] # Canonical
 }
 
-# IAM role
-resource "aws_iam_role" "beacon" {
-  name = "${var.project}_beacon"
-
-  assume_role_policy = <<EOF
-{
-  "Version": "2012-10-17",
-  "Statement": [
-    {
-      "Effect": "Allow",
-      "Principal": {
-        "Service": "ec2.amazonaws.com"
-      },
-      "Action": "sts:AssumeRole"
-    }
-  ]
-}
-EOF
-}
-
-# IAM profile
-resource "aws_iam_instance_profile" "beacon" {
-  name = "${var.project}_beacon"
-  role = "${aws_iam_role.beacon.name}"
-}
-
 # Instance
-resource "aws_instance" "beacon" {
-  ami = "${data.aws_ami.ubuntu.id}"
-  instance_type = "t3.micro"
-  key_name = "${var.key_name}"
+resource "aws_instance" "eth" {
+  ami                    = "${data.aws_ami.ubuntu.id}"
+  instance_type          = "c5.large"
+  key_name               = "${var.key_name}"
   vpc_security_group_ids = var.security_group_ids
-  subnet_id = "${var.subnet_id}"
-  iam_instance_profile = "${aws_iam_instance_profile.beacon.name}"
+  subnet_id              = "${var.subnet_id}"
+  iam_instance_profile   = var.iam_instance_profile
 
   tags = {
     project = "${var.project}"
-    role = "beacon"
+    role    = "eth"
   }
 
   volume_tags = {
     project = "${var.project}"
-    role = "beacon"
+    role    = "eth"
   }
 }
