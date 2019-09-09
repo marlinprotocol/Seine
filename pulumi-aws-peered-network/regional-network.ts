102026b768017b854a588349d027dbd7daaccdda
Roshan Raghupathy <thisisroshansmail@gmail.com>
2019-09-09 14:36:20 +0530

    Migrate inputs from tf

--- pulumi-aws-peered-network/regional-network.ts
+++ pulumi-aws-peered-network/regional-network.ts
@@ -1,9 +1,14 @@
 import * as pulumi from "@pulumi/pulumi";
 import * as aws from "@pulumi/aws";
 
+interface RegionalNetworkArgs {
+    vpcCidr: pulumi.Input<string>;
+    project: string;
+}
+
 export class RegionalNetwork extends pulumi.ComponentResource {
 
-    constructor(name: string, args: {}, opts?: pulumi.ComponentResourceOptions) {
+    constructor(name: string, args: RegionalNetworkArgs, opts?: pulumi.ComponentResourceOptions) {
         super("marlin:AWSRegionalNetwork", name, {}, opts);
     }
 }
