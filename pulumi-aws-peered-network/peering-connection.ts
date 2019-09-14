753b869003c11d6473ce9f117f92b2af8b0be3b5
Roshan Raghupathy <thisisroshansmail@gmail.com>
2019-09-14 19:23:25 +0530

    Migrate inputs from tf

--- pulumi-aws-peered-network/peering-connection.ts
+++ pulumi-aws-peered-network/peering-connection.ts
@@ -1,6 +1,18 @@
 import * as pulumi from "@pulumi/pulumi";
 import * as aws from "@pulumi/aws";
 
+interface PeeringConnectionArgs {
+    srcVpcId: pulumi.Input<string>;
+    dstVpcId: pulumi.Input<string>;
+    srcRouteTableId: pulumi.Input<string>;
+    dstRouteTableId: pulumi.Input<string>;
+    srcCidr: pulumi.Input<string>;
+    dstCidr: pulumi.Input<string>;
+    srcRegion: aws.Region;
+    dstRegion: aws.Region;
+    tags?: pulumi.Input<{[key: string]: any}>;
+}
+
 export class PeeringConnection extends pulumi.ComponentResource {
 
     constructor(name: string, args: PeeringConnectionArgs, opts?: pulumi.ComponentResourceOptions) {
