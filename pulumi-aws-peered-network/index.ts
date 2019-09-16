ab9aaeec1244a4ef82bbda40d6d67457c99a4960
Roshan Raghupathy <thisisroshansmail@gmail.com>
2019-09-16 16:54:22 +0530

    Migrate inputs from tf

--- pulumi-aws-peered-network/index.ts
+++ pulumi-aws-peered-network/index.ts
@@ -2,6 +2,10 @@ import * as pulumi from "@pulumi/pulumi";
 import * as aws from "@pulumi/aws";
 import * as awsx from "@pulumi/awsx";
 
+interface AWSPeeredNetworkArgs {
+    tags?: pulumi.Input<{[key: string]: any}>;
+}
+
 export class AWSPeeredNetwork extends pulumi.ComponentResource {
 
     constructor(name: string, args: AWSPeeredNetworkArgs, opts?: pulumi.ComponentResourceOptions) {
