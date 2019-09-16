665c6dc72eb1bf9a5ba4ca28f74fda35ea7c34bf
Roshan Raghupathy <thisisroshansmail@gmail.com>
2019-09-16 14:30:52 +0530

    Add basic component for network module

--- pulumi-aws-peered-network/index.ts
+++ pulumi-aws-peered-network/index.ts
@@ -1,3 +1,10 @@
 import * as pulumi from "@pulumi/pulumi";
 import * as aws from "@pulumi/aws";
 import * as awsx from "@pulumi/awsx";
+
+export class AWSPeeredNetwork extends pulumi.ComponentResource {
+
+    constructor(name: string, args: AWSPeeredNetworkArgs, opts?: pulumi.ComponentResourceOptions) {
+        super("marlin:AWSPeeredNetwork", name, {}, opts);
+    }
+}
