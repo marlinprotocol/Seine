f6c0d7c7735bb1c738b65291851f32b6604a12e9
Roshan Raghupathy <thisisroshansmail@gmail.com>
2019-09-16 18:34:07 +0530

    Migrate outputs from tf

--- pulumi-aws-peered-network/index.ts
+++ pulumi-aws-peered-network/index.ts
@@ -2,11 +2,14 @@ import * as pulumi from "@pulumi/pulumi";
 import * as aws from "@pulumi/aws";
 import * as awsx from "@pulumi/awsx";
 
+import { RegionalNetwork } from "./regional-network"
+
 interface AWSPeeredNetworkArgs {
     tags?: pulumi.Input<{[key: string]: any}>;
 }
 
 export class AWSPeeredNetwork extends pulumi.ComponentResource {
+    public readonly children: {[key: string]: RegionalNetwork};
 
     constructor(name: string, args: AWSPeeredNetworkArgs, opts?: pulumi.ComponentResourceOptions) {
         super("marlin:AWSPeeredNetwork", name, {}, opts);
