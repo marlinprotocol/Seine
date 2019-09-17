879e8dc0daf9c5d07e0e87f7d4959d58794c8841
Roshan Raghupathy <thisisroshansmail@gmail.com>
2019-09-17 15:19:54 +0530

    Add peering connections between the VPCs

--- pulumi-aws-peered-network/index.ts
+++ pulumi-aws-peered-network/index.ts
@@ -3,6 +3,7 @@ import * as aws from "@pulumi/aws";
 import * as awsx from "@pulumi/awsx";
 
 import { RegionalNetwork } from "./regional-network"
+import { PeeringConnection } from "./peering-connection"
 
 interface IAWSNewVpcArgs {
     name: string;
@@ -22,6 +23,7 @@ export class AWSPeeredNetwork extends pulumi.ComponentResource {
         super("marlin:AWSPeeredNetwork", name, {}, opts);
 
         this.children = {};
+        let processed: IAWSNewVpcArgs[] = [];
         for(let vpcCidr of args.vpcCidrs) {
             let rn = new RegionalNetwork(vpcCidr.name, {
                 vpcCidr: vpcCidr.cidrBlock,
@@ -30,6 +32,25 @@ export class AWSPeeredNetwork extends pulumi.ComponentResource {
                 parent: this,
             });
 
+            // Peer with other rn
+            for(let peerVpcBlock of processed) {
+                let prn = this.children[peerVpcBlock.name];
+                new PeeringConnection(`${vpcBlock.name}-${peerVpcBlock.name}`, {
+                    srcVpcId: rn.vpc.id,
+                    dstVpcId: prn.vpc.id,
+                    srcRouteTableId: rn.routeTable.id,
+                    dstRouteTableId: prn.routeTable.id,
+                    srcCidr: rn.vpc.cidrBlock,
+                    dstCidr: prn.vpc.cidrBlock,
+                    srcRegion: vpcBlock.region,
+                    dstRegion: peerVpcBlock.region,
+                    tags: args.tags,
+                }, {
+                    parent: this,
+                });
+            }
+
+            processed.push(vpcBlock);
             this.children[vpcCidr.name] = rn;
         }
     }
