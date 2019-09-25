deac0d11b9ccd0adab05fba9f695dcc0ce4ce3b5
Roshan Raghupathy <thisisroshansmail@gmail.com>
2019-09-25 13:43:48 +0530

    Add base component to deploy instances in all regions

--- pulumi-aws-parity-private-network/index.ts
+++ pulumi-aws-parity-private-network/index.ts
@@ -1,3 +1,10 @@
 import * as pulumi from "@pulumi/pulumi";
 import * as aws from "@pulumi/aws";
 import * as awsx from "@pulumi/awsx";
+
+export class AWSParityPrivateNetwork extends pulumi.ComponentResource {
+
+    constructor(name: string, args: AWSParityPrivateNetworkArgs, opts?: pulumi.ComponentResourceOptions) {
+        super("marlin:AWSParityPrivateNetwork", name, {}, opts);
+    }
+}
