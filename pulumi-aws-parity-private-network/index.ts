d14d5816ccd1aaf57c6d96fd101f76643d5b92f1
Roshan Raghupathy <thisisroshansmail@gmail.com>
2019-09-25 14:28:58 +0530

    Migrate inputs from tf

--- pulumi-aws-parity-private-network/index.ts
+++ pulumi-aws-parity-private-network/index.ts
@@ -2,6 +2,15 @@ import * as pulumi from "@pulumi/pulumi";
 import * as aws from "@pulumi/aws";
 import * as awsx from "@pulumi/awsx";
 
+interface AWSParityPrivateNetworkArgs {
+    subnets: {[key: string]: pulumi.Input<aws.ec2.Subnet>};
+    instanceType: pulumi.Input<aws.ec2.InstanceType>;
+    keyName: pulumi.Input<string>;
+    egress?: aws.types.input.ec2.SecurityGroupEgress[];
+    ingress?: aws.types.input.ec2.SecurityGroupIngress[];
+    tags?: pulumi.Input<{[key: string]: any}>;
+}
+
 export class AWSParityPrivateNetwork extends pulumi.ComponentResource {
 
     constructor(name: string, args: AWSParityPrivateNetworkArgs, opts?: pulumi.ComponentResourceOptions) {
