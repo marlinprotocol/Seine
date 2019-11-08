ebe4f6a42d4f458050a62bbe9123a7370a69fcdf
Roshan Raghupathy <thisisroshansmail@gmail.com>
2019-11-08 14:19:32 +0530

    Add inputs based on AWS inputs, don't need peering connections on GCP

--- global-network/gcp/pulumi/index.ts
+++ global-network/gcp/pulumi/index.ts
@@ -1,6 +1,11 @@
 import * as pulumi from "@pulumi/pulumi";
 import * as gcp from "@pulumi/gcp";
 
+interface GCPGlobalNetworkArgs {
+    subnets: {[key: string]: {region: string, cidr: string}};
+    firewalls?: {[key: string]: Omit<gcp.compute.FirewallArgs, "network">};
+}
+
 export class GCPGlobalNetwork extends pulumi.ComponentResource {
 
     constructor(name: string, args: GCPGlobalNetworkArgs, opts?: pulumi.ComponentResourceOptions) {
