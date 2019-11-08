bbb4083e21fa966d7591bee06165209299edb862
Roshan Raghupathy <thisisroshansmail@gmail.com>
2019-11-08 14:14:11 +0530

    Add basic component for global network

--- global-network/gcp/pulumi/index.ts
+++ global-network/gcp/pulumi/index.ts
@@ -1,2 +1,9 @@
 import * as pulumi from "@pulumi/pulumi";
 import * as gcp from "@pulumi/gcp";
+
+export class GCPGlobalNetwork extends pulumi.ComponentResource {
+
+    constructor(name: string, args: GCPGlobalNetworkArgs, opts?: pulumi.ComponentResourceOptions) {
+        super("marlin:GCP:GlobalNetwork", name, {}, opts);
+    }
+}
