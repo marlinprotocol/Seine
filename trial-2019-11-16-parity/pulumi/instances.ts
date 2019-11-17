7b09a14c56c6097094f8fdcda3c803ec8a601976
Roshan Raghupathy <thisisroshansmail@gmail.com>
2019-11-17 16:48:59 +0530

    Provision instances, lots of parameters missing

--- trial-2019-11-16-parity/pulumi/instances.ts
+++ trial-2019-11-16-parity/pulumi/instances.ts
@@ -11,5 +11,36 @@ export class GCPInstances extends pulumi.ComponentResource {
 
     constructor(name: string, args: GCPInstancesArgs, opts?: pulumi.ComponentResourceOptions) {
         super("marlin:GCP:Instances", name, {}, opts);
+
+        this.instances = {};
+
+        for(let subnetName in args.subnets) {
+            let subnet = args.subnets[subnetName];
+
+            this.instances[`${subnetName}-${name}`] = pulumi.output(subnet).apply(subnet => {
+                return new gcp.compute.Instance(`${subnetName}-${name}`, {
+                    allowStoppingForUpdate: false,
+                    bootDisk: {
+                        initializeParams: {
+                            image: "ubuntu-1804-lts",
+                            labels: args.labels,
+                        }
+                    },
+                    labels: args.labels,
+                    machineType: "",
+                    networkInterfaces: [{
+                        accessConfigs: [{}],
+                        subnetwork: subnet.name,
+                    }],
+                    scheduling: [{
+                        interface: "NVME",
+                    }],
+                    tags: [],
+                    zone: subnet.region.apply(region => { return `${region}-c`; }),
+                }, {
+                    parent: subnet,
+                });
+            });
+        }
     }
 }
