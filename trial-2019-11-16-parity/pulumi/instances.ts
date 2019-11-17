42792e8e6eca1a6f16f702b9842f0db245f9bf16
Roshan Raghupathy <thisisroshansmail@gmail.com>
2019-11-17 23:02:52 +0530

    Add count parameter and provision multiple instances in same subnet

--- trial-2019-11-16-parity/pulumi/instances.ts
+++ trial-2019-11-16-parity/pulumi/instances.ts
@@ -4,6 +4,7 @@ import * as gcp from "@pulumi/gcp";
 interface GCPInstancesArgs {
     subnets: {[key: string]: pulumi.Input<gcp.compute.Subnetwork>};
     instanceType: string;
+    count: number;
     preemptible?: boolean;
     localssd?: boolean;
     networkTags?: pulumi.Input<pulumi.Input<string>[]>;
@@ -21,34 +22,36 @@ export class GCPInstances extends pulumi.ComponentResource {
         for(let subnetName in args.subnets) {
             let subnet = args.subnets[subnetName];
 
-            this.instances[`${subnetName}-${name}`] = pulumi.output(subnet).apply(subnet => {
-                return new gcp.compute.Instance(`${subnetName}-${name}`, {
-                    allowStoppingForUpdate: false,
-                    bootDisk: {
-                        initializeParams: {
-                            image: "ubuntu-1804-lts",
-                            labels: args.labels,
-                        }
-                    },
-                    labels: args.labels,
-                    machineType: args.instanceType,
-                    networkInterfaces: [{
-                        accessConfigs: [{}],
-                        subnetwork: subnet.name,
-                    }],
-                    scheduling: args.preemptible ? {
-                        automaticRestart: false,
-                        preemptible: true,
-                    } : undefined,
-                    scratchDisks: args.localssd ? [{
-                        interface: "NVME",
-                    }] : undefined,
-                    tags: args.networkTags,
-                    zone: subnet.region.apply(region => { return `${region}-c`; }),
-                }, {
-                    parent: subnet,
+            for (let idx = 0; idx < args.count; ++idx) {
+                this.instances[`${subnetName}-${name}-${idx+1}`] = pulumi.output(subnet).apply(subnet => {
+                    return new gcp.compute.Instance(`${subnetName}-${name}-${idx+1}`, {
+                        allowStoppingForUpdate: false,
+                        bootDisk: {
+                            initializeParams: {
+                                image: "ubuntu-1804-lts",
+                                labels: args.labels,
+                            }
+                        },
+                        labels: args.labels,
+                        machineType: args.instanceType,
+                        networkInterfaces: [{
+                            accessConfigs: [{}],
+                            subnetwork: subnet.name,
+                        }],
+                        scheduling: args.preemptible ? {
+                            automaticRestart: false,
+                            preemptible: true,
+                        } : undefined,
+                        scratchDisks: args.localssd ? [{
+                            interface: "NVME",
+                        }] : undefined,
+                        tags: args.networkTags,
+                        zone: subnet.region.apply(region => { return `${region}-c`; }),
+                    }, {
+                        parent: subnet,
+                    });
                 });
-            });
+            }
         }
     }
 }
