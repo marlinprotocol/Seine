e54c40cfedd0e6ad8a78bd8555398d310435cdd4
Roshan Raghupathy <thisisroshansmail@gmail.com>
2019-11-17 16:42:59 +0530

    Add basic inputs

--- trial-2019-11-16-parity/pulumi/instances.ts
+++ trial-2019-11-16-parity/pulumi/instances.ts
@@ -1,6 +1,11 @@
 import * as pulumi from "@pulumi/pulumi";
 import * as gcp from "@pulumi/gcp";
 
+interface GCPInstancesArgs {
+    subnets: {[key: string]: pulumi.Input<gcp.compute.Subnetwork>};
+    labels?: pulumi.Input<{[key: string]: any}>;
+}
+
 export class GCPInstances extends pulumi.ComponentResource {
     public readonly instances: {[key: string]: pulumi.Output<gcp.compute.Instance>};
 
