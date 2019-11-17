f434742b4e3f53d24dc87ac28390d7647bede6ef
Roshan Raghupathy <thisisroshansmail@gmail.com>
2019-11-17 14:03:22 +0530

    Export instances

--- trial-2019-11-16-parity/pulumi/instances.ts
+++ trial-2019-11-16-parity/pulumi/instances.ts
@@ -2,6 +2,8 @@ import * as pulumi from "@pulumi/pulumi";
 import * as gcp from "@pulumi/gcp";
 
 export class GCPInstances extends pulumi.ComponentResource {
+    public readonly instances: {[key: string]: pulumi.Output<gcp.compute.Instance>};
+
     constructor(name: string, args: GCPInstancesArgs, opts?: pulumi.ComponentResourceOptions) {
         super("marlin:GCP:Instances", name, {}, opts);
     }
