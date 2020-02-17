c267ae7ca9707f006245c01534fdf2aff6dd1fd9
Roshan Raghupathy <thisisroshansmail@gmail.com>
2020-02-17 16:45:53 +0530

    Add basic relay network class with a global network within

--- provisioning/marlin-relay-network/gcp/pulumi/index.ts
+++ provisioning/marlin-relay-network/gcp/pulumi/index.ts
@@ -1,8 +1,26 @@
 import * as pulumi from "@pulumi/pulumi";
 import * as gcp from "@pulumi/gcp";
 
-// Create a GCP resource (Storage Bucket)
-const bucket = new gcp.storage.Bucket("my-bucket");
+import { GCPGlobalNetwork } from "@marlinlabs/pulumi-gcp-global-network"
 
-// Export the DNS name of the bucket
-export const bucketName = bucket.url;
+
+interface GCPRelayNetworkArgs {
+    beacon_subnets: {[key: string]: {region: string, cidr: string}};
+    monitoring_subnets: {[key: string]: {region: string, cidr: string}};
+    relay_subnets: {[key: string]: {region: string, cidr: string}};
+    firewalls?: {[key: string]: Omit<gcp.compute.FirewallArgs, "network">};
+    labels?: pulumi.Input<{[key: string]: any}>;
+}
+
+export class GCPRelayNetwork extends pulumi.ComponentResource {
+    readonly network: GCPGlobalNetwork;
+
+    constructor(name: string, args: GCPRelayNetworkArgs, opts?: pulumi.ComponentResourceOptions) {
+        super("marlin:GCP:RelayNetwork", name, {}, opts);
+
+        let subnets = {...args.beacon_subnets, ...args.monitoring_subnets, ...args.relay_subnets}
+        this.network = new GCPGlobalNetwork(`${name}-globalnet`, {
+            subnets: subnets,
+        });
+    }
+}
