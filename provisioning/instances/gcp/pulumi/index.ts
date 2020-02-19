234ddb4fea5911ec3f1d17f3839933b850ef3918
Roshan Raghupathy <thisisroshansmail@gmail.com>
2020-02-19 22:53:37 +0530

    Copy instance provisioning from old files

--- provisioning/instances/gcp/pulumi/index.ts
+++ provisioning/instances/gcp/pulumi/index.ts
@@ -1,8 +1,57 @@
 import * as pulumi from "@pulumi/pulumi";
 import * as gcp from "@pulumi/gcp";
 
-// Create a GCP resource (Storage Bucket)
-const bucket = new gcp.storage.Bucket("my-bucket");
+interface GCPInstancesArgs {
+    subnets: {[key: string]: pulumi.Input<gcp.compute.Subnetwork>};
+    instanceType: string;
+    count: number;
+    preemptible?: boolean;
+    localssd?: boolean;
+    networkTags?: pulumi.Input<pulumi.Input<string>[]>;
+    labels?: pulumi.Input<{[key: string]: any}>;
+}
 
-// Export the DNS name of the bucket
-export const bucketName = bucket.url;
+export class GCPInstances extends pulumi.ComponentResource {
+    public readonly instances: {[key: string]: pulumi.Output<gcp.compute.Instance>};
+
+    constructor(name: string, args: GCPInstancesArgs, opts?: pulumi.ComponentResourceOptions) {
+        super("marlin:GCP:Instances", name, {}, opts);
+
+        this.instances = {};
+
+        for(let subnetName in args.subnets) {
+            let subnet = args.subnets[subnetName];
+
+            for (let idx = 0; idx < args.count; ++idx) {
+                this.instances[`${subnetName}-${name}-${idx+1}`] = pulumi.output(subnet).apply(subnet => {
+                    return new gcp.compute.Instance(`${subnetName}-${name}-${idx+1}`, {
+                        allowStoppingForUpdate: false,
+                        bootDisk: {
+                            initializeParams: {
+                                image: "ubuntu-1804-lts",
+                                labels: args.labels,
+                            }
+                        },
+                        labels: args.labels,
+                        machineType: args.instanceType,
+                        networkInterfaces: [{
+                            accessConfigs: [{}],
+                            subnetwork: subnet.name,
+                        }],
+                        scheduling: args.preemptible ? {
+                            automaticRestart: false,
+                            preemptible: true,
+                        } : undefined,
+                        scratchDisks: args.localssd ? [{
+                            interface: "NVME",
+                        }] : undefined,
+                        tags: args.networkTags,
+                        zone: subnet.region.apply(region => { return `${region}-c`; }),
+                    }, {
+                        parent: subnet,
+                    });
+                });
+            }
+        }
+    }
+}
