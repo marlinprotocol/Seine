3984e420a740f839e610db67a600e423cb811203
Roshan Raghupathy <thisisroshansmail@gmail.com>
2020-06-02 18:37:49 +0530

    Set up for eth

--- orchestration/katara/index.ts
+++ orchestration/katara/index.ts
@@ -5,7 +5,7 @@ import { GCPRelayNetwork } from "@marlinlabs/katara-gcp-pulumi"
 import { GCPInstances } from "@marlinlabs/pulumi-gcp-instances"
 
 let labels = {
-    "project": "someproject",
+    "project": "eth",
     "manager": "pulumi",
 };
 
@@ -37,29 +37,29 @@ regions.map((region, idx) => {
 });
 
 delete subnets["asia-east2"];
-// delete subnets["asia-northeast1"];
+delete subnets["asia-northeast1"];
 delete subnets["asia-south1"];
-// delete subnets["asia-southeast1"];
+delete subnets["asia-southeast1"];
 delete subnets["australia-southeast1"];
 delete subnets["europe-north1"];
-// delete subnets["europe-west2"];
-// delete subnets["europe-west3"];
+delete subnets["europe-west2"];
+delete subnets["europe-west3"];
 delete subnets["europe-west6"];
 delete subnets["northamerica-northeast1"];
 delete subnets["southamerica-east1"];
 delete subnets["us-central1"];
-// delete subnets["us-east1"];
+delete subnets["us-east1"];
 delete subnets["us-east4"];
 // delete subnets["us-west1"];
 delete subnets["us-west2"];
 
 let relayNetwork = new GCPRelayNetwork(`${labels["project"]}`, {
     beaconSubnets: {"us-west1": {...subnets["us-west1"], count: 1}},
-    monitoringSubnets: {"us-west1": {...subnets["us-west1"], count: 1}},
+    monitoringSubnets: {"us-west1": {...subnets["us-west1"], count: 0}},
     relaySubnets: Object.keys(subnets).reduce((o, key) => {
         return {
             ...o,
-            [key]: {...subnets[key], count: 2},
+            [key]: {...subnets[key], count: 0},
         };
     }, {}),
     labels: labels,
@@ -69,7 +69,7 @@ let masterInstances = new GCPInstances(`${labels["project"]}-masters`, {
     subnets: Object.keys(subnets).reduce((o, key) => {
         return {
             ...o,
-            [key]: {subnet: relayNetwork.network.subnets[key], count: 1},
+            [key]: {subnet: relayNetwork.network.subnets[key], count: 0},
         };
     }, {}),
     instanceType: "n1-standard-1",
@@ -88,23 +88,23 @@ let masterIps = Object.values(masterInstances.instances).map((i) => {
     });
 });
 
-let msggenInstances = new GCPInstances(`${labels["project"]}-msggens`, {
+let ethInstances = new GCPInstances(`${labels["project"]}-eths`, {
     subnets: Object.keys(subnets).reduce((o, key) => {
         return {
             ...o,
-            [key]: {subnet: relayNetwork.network.subnets[key], count: 4},
+            [key]: {subnet: relayNetwork.network.subnets[key], count: 0},
         };
     }, {}),
     instanceType: "n1-standard-1",
     localssd: true,
-    networkTags: ["msggen"],
+    networkTags: ["eth"],
     labels: {
         ...labels,
-        "role": "msggen",
+        "role": "eth",
     },
 });
 
-let msggenIps = Object.values(msggenInstances.instances).map((i) => {
+let ethIps = Object.values(ethInstances.instances).map((i) => {
     return i.apply((i) => {
         return i.networkInterfaces.apply((i) => {
             return i[0].networkIp;
